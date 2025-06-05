1a25c399e0f01a65d72257d43a20e0efe4092441
colorincode <colorincodeco@gmail.com>
2025-06-05 11:28:08 -0600

    final changes

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -88,6 +88,7 @@ let smoother = ScrollSmoother.create({
   wrapper: "#smooth-wrapper",
   content: "#smooth-content",
   smooth: 2,
+  speed: 1,
   effects: true,
   normalizeScroll: true,
 });
@@ -151,6 +152,22 @@ function closeMobileMenu() {
   navBar.classList.remove("active");
 
 }
+
+// let bodyScrollBar = Scrollbar.init(document.body, { damping: 0.1, delegateTo: document });
+ 
+// bodyScrollBar.setPosition(0, 0);
+// bodyScrollBar.track.xAxis.element.remove();
+
+// ScrollTrigger.scrollerProxy(document.body, {
+//   scrollTop(value) {
+//     if (arguments.length) {
+//       bodyScrollBar.scrollTop = value;
+//     }
+//     return bodyScrollBar.scrollTop;
+//   }
+// });
+
+// bodyScrollBar.addListener(ScrollTrigger.update);
 
 
 function setupSmoothAnchors() {
